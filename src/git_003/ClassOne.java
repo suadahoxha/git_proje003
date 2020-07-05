28aaa509567c95b8960b15171d3dff57fb0a9c89
suadahoxha <apple@apple-MacBook-Pro.local>
2020-07-05 14:06:49 -0400

    ucuncu commit yapildi

--- src/git_003/ClassOne.java
+++ src/git_003/ClassOne.java
@@ -5,7 +5,8 @@ public class ClassOne {
 	public static void main(String[] args) {
 		System.out.println("local master branch");
 		System.out.println("locad master branch ikinci commit");
-
+		System.out.println("local 3.master branch");
+		System.out.println("locad master branch 3. commit");
 	}
 
 }
